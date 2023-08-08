8187d04db38085d8c4af9825dea7d0d3dc1cd7fe
Ian Ibnu Wahab <iandroid.saimima@gmail.com>
2023-08-08 11:26:52 +0800

    UPDATE : hapus enum PopupStyle.dialogScreenModal

--- lib/pages/searchable_dropdown.dart
+++ lib/pages/searchable_dropdown.dart
@@ -2,7 +2,7 @@
 
 import 'package:flutter/material.dart';
 
-enum PopupStyle { bottomSheetModal, fullScreenModal, dialogScreenModal }
+enum PopupStyle { bottomSheetModal, fullScreenModal}
 
 class SearchableDropdown<T> extends StatefulWidget {
   final List<T> items;
